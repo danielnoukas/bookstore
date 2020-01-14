ae629de8b186843f933e1aca073b2fd25dead81d
danielnoukas <daniel.noukas@gmail.com>
2020-01-14 11:18:12 +0200

    eauf

--- edit.php
+++ edit.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'db_connection.php';
+var_dump($_GET);
 $id = $_GET['id'];
 $stmt = $pdo->prepare('SELECT * FROM Books.books b LEFT JOIN book_authors ba ON ba.book_id = b.id
 LEFT JOIN authors a ON ba.author_id = a.id WHERE b.id = :id');
@@ -28,27 +29,13 @@ body {
 <h3>Raamatu pealkiri</h3>
 <?php echo $book['title']; ?>  
 <br>
-<h3>Raamatu autor</h3>
-<?php echo $book['first_name']; ?> <?php echo $book['last_name']; ?>
 <h3>Raamatu ilmumisaasta</h3>
-<?php echo $book['release_date']; ?>  
-<br>
-<h3>Raamatu keel</h3>
-<?php echo $book['language']; ?>  
-<br>
-<h3>Raamatu kirjeldus</h3>
-<?php echo $book['summary']; ?>  
-<br>
-<h3>Raamatu hind</h3>
-<?php echo $book['price']; ?>
-<br> 
-<h3>Raamatu lehekülgede arv</h3> 
-<?php echo $book['pages']; ?> 
-<br>
-<img src="<?php echo $book['cover_path']; ?>" alt="Smiley face" height="150" width="150">
-<br>
-<a href="delete.php?id=<?php echo $id; ?>">Delete</a>
-<br>
-<a href="edit.php?id=<?php echo $id; ?>">Edit</a>
+<form action="edit.php" method="get">
+       
+        <input type='text' name='year' value='<?php echo $book['release_date']; ?>'>
+        <br>
+        <input type='submit' value='Salvesta'>
+    </form>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 
 <?php
-
-$host = '127.0.0.1';
-$db   = 'Books';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-
+require_once 'db_connection.php';
 //var_dump($_GET);
 $title = $_GET['title'];
 $year = $_GET['year'];
@@ -28,7 +10,6 @@ LEFT JOIN book_authors ON books.id=book_authors.book_id
 LEFT JOIN authors ON authors.id=book_authors.author_id
 WHERE title LIKE :title AND release_date= :year');
 $stmt->execute (['title'=> '%' . $title . '%', 'year' => $year]);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,22 +20,17 @@ $stmt->execute (['title'=> '%' . $title . '%', 'year' => $year]);
     <title>Document</title>
 </head>
 <body>
-
+<body style='background-color:#e6e5ca'>
     <h1>Otsing</h1>
-    <?php
-    require_once 'db_connection.php';
-
-    $year = $_GET['year'];
-    $title = $_GET['title'];
-    
-    ?>
 
 
     <form action="index.php" method="get">
        
         <input type='text' name='title' placeholder='Pealkiri'>
         <br>
+        <br>
         <input type='text' name='year' placeholder='Aasta'>
+        <br>
         <br>
         <input type='submit' value='Otsi'>
     </form>
@@ -63,7 +39,6 @@ $stmt->execute (['title'=> '%' . $title . '%', 'year' => $year]);
 
 
 <?php
-
     $stmt = $pdo->prepare('SELECT * FROM books WHERE release_date LIKE :year AND title LIKE :title');
     $stmt->execute(['year' => '%' . $year . '%', 'title' => '%' . $title . '%']);
     
