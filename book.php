e9ab42cda5435dea63284863bc7b3f0df6617daf
danielnoukas <daniel.noukas@gmail.com>
2020-01-22 09:56:22 +0200

    sada

--- book.php
+++ book.php
@@ -1,12 +1,12 @@
 <?php
 require_once 'db_connection.php';
-
 $id = $_GET['id'];
-$stmt = $pdo->prepare('SELECT * FROM books WHERE id=:id');
+$stmt = $pdo->prepare('SELECT * FROM Books.books b LEFT JOIN book_authors ba ON ba.book_id = b.id
+LEFT JOIN authors a ON ba.author_id = a.id WHERE b.id = :id');
 $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
-?>
 
+?>
 
 
 <!DOCTYPE html>
@@ -15,9 +15,27 @@ $book = $stmt->fetch();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=$book['title'] ?></title>
+    <title><?= $book['title']; ?></title>
+
+    
+    
 </head>
 <body>
-    
+<a href="delete.php?id=<?php echo $id;  ?>">DELETE</a>
+<a href="edit.php?id=<?php echo $id;  ?>">EDIT</a>
+<br>
+<?php  echo $book['title']; ?>  
+<br>
+<?php echo $book['release_date']; ?>  
+<br>
+<?php echo $book['language']; ?>  
+<br>
+<?php echo $book['summary']; ?>  
+<br>
+<?php echo $book['price']; ?>
+<br> 
+<?php echo $book['pages']; ?> 
+<br> 
+<?php echo $book['first_name'] ." ". $book['last_name']; ?> 
 </body>
 </html>
